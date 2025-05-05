ed1ecce74b6eb01305b26f995fd78d90788787bf
AyeletMax <Hand4d20@mbjcomp.org.il>
2025-05-06 01:01:06 +0300

    gsdh

--- Server/service/todosService.js
+++ Server/service/todosService.js
@@ -3,5 +3,5 @@ export function queryAllTodos () {
     db.query('SELECT * FROM todos', (err, result) => {
         if (err) throw err;
         console.log("comments table created");
-    });
+        console.log("hello")    });
 }
